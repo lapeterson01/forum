5ad15abc0c0f2496b13024fb4d0709732e8efaa4
Logan Peterson <lapeterson01@gmail.com>
2018-01-17 09:52:14 -0500

    Initial

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// your code here
+var button = document.getElementsByTagName('button')[0];
+button.addEventListener('click', function() {
+  var userName = document.getElementsByClassName('form-control')[0].value;
+  var nameList = document.createElement('li');
+  var newName = document.createTextNode(userName + ":");
+  nameList.appendChild(newName);
+  document.getElementsByClassName('posts')[0].append(nameList);
+  //posts name to forum
+  var userWords = document.getElementsByClassName('form-control')[1].value;
+  var wordsList = document.createElement('li');
+  var newWords = document.createTextNode(userWords);
+  wordsList.appendChild(newWords);
+  document.getElementsByClassName('posts')[0].append(wordsList);
+  //posts message to forum
+  var blank = document.createElement('li');
+  var newBlank = document.createTextNode('');
+  blank.appendChild(newBlank);
+  document.getElementsByClassName('posts')[0].append(blank);
+  //adds a space between each post
+});
